ccee9e57192c06aaa63e7032121e97bf73bb7fdc
DaryaEgorova23 <dusy23051996@yandex.ru>
2023-08-06 13:15:02 +0300

    first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://3c88e56f-eb84-4afe-8f5f-bb03151bab4f.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH= "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
